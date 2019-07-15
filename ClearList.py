62106cd050340c05aea93606da61fa4e5626ac4e
toor <k.nosek@ievent-management.pl>
2019-07-15 23:59:03 +0000

    new knowledge 2.0

---
+++ ClearList.py
@@ -0,0 +1,19 @@
+
+# CLEAR
+first_list = [1, 2, 3]
+first_list.clear()
+print(first_list)
+
+# POP
+second_list = ["Marek", "Fiona", "Witkacy", "Eustachy"]
+second_list.pop()
+print(second_list)
+
+# POP - other useful
+other_list = second_list.pop()
+print(other_list)
+
+# REMOVE
+third_list = ["Wszystko", "dzis", "nie", "mam", "pod", "kontrola"]
+third_list.remove("nie")
+print(third_list)
